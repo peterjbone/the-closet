2e2d56459f154743bcce8bd27ed1701c081257f8
peterjbone <peterjbone41@gmail.com>
2024-02-20 13:16:28 -0500

    Archivos index.js y server.js creados en server

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const server = express();
+const morgan = require("morgan");
+
+server.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Credentials", "true");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	next();
+});
+
+server.use(express.json());
+server.use(morgan("dev"));
+
+//* Routes 👇
+
+module.exports = server;
