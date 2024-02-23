40472c0708e6bb2ea0382b204e338c00c512fd58
peterjbone <peterjbone41@gmail.com>
2024-02-23 12:10:21 -0500

    Terminado gran parte de la UI de la homepage

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import "./styles.css";
 import HeroSection from "./components/heroSection/HeroSection";
 import CategoryBanner from "./components/categoryBanner/CategoryBanner";
 import FeaturedProducts from "./components/featuredProducts/FeaturedProducts";
+import Collections from "./components/collections/Collections";
+import TrendProducts from "./components/trendProducts/TrendProducts";
 
 function Home() {
 	/* prettier-ignore*/
@@ -10,7 +12,9 @@ function Home() {
 		<>
       <HeroSection />	
       <CategoryBanner /> 
-      <FeaturedProducts/>
+      <FeaturedProducts />
+      <Collections />
+      <TrendProducts/>
 		</>
 	);
 }
