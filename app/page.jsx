9da63cea111c78ecc15b2fcb9366f1672ab59699
peterjbone <peterjbone41@gmail.com>
2024-02-23 12:59:14 -0500

    Eliminando una mala importación en app/page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,3 @@
-import "./styles.css";
-
 import HeroSection from "./components/heroSection/HeroSection";
 import CategoryBanner from "./components/categoryBanner/CategoryBanner";
 import FeaturedProducts from "./components/featuredProducts/FeaturedProducts";
