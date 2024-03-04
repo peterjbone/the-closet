69d5140b5134176232e3f8c38e56a9c1413fb62e
Abril31 <brivaldi0416@gmail.com>
2024-03-04 10:55:07 -0400

    Modificacion Detail y productos.js para insertar correctamente la propiedad de descripcion.

--- backend/models/Productos.js
+++ backend/models/Productos.js
@@ -1,26 +1,26 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
-	nombre: String,
-	marca: String,
-	description: String,
-	precio: Number,
-	oferta: {
-		type: mongoose.Schema.Types.Mixed,
-		default: {}
-	},
-	Activo: Boolean,
-	productoNuevo: Boolean,
-	categoria: String,
-	genero: String,
-	subcategoria: String,
-	imagen: [String],
-	tallas: [String],
-	colores: [String],
-	opcion: {
-		type: mongoose.Schema.Types.Mixed,
-		default: []
-	}
+  nombre: String,
+  marca: String,
+  descripcion: String, // Cambiado a minúsculas pq no se insertó a la base de datos.
+  precio: Number,
+  oferta: {
+    type: mongoose.Schema.Types.Mixed,
+    default: {},
+  },
+  activo: Boolean,
+  productoNuevo: Boolean,
+  categoria: String,
+  genero: String,
+  subcategoria: String,
+  imagen: [String],
+  tallas: [String],
+  colores: [String],
+  opcion: {
+    type: mongoose.Schema.Types.Mixed,
+    default: [],
+  },
 });
 
 module.exports = mongoose.model("Producto", ProductSchema);
